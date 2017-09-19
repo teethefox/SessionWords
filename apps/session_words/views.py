fbbe2bd65b308a4d5fbc4a20113097b74ea625c1
Tiffani Fox <tiffanipack@Tiffanipack.local>
2017-09-19 14:57:53 -0700

    Session Words Assignment

---
+++ apps/session_words/views.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals
+from datetime import datetime
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def process(request):
+    new_word = {}
+    for key, value in request.POST.iteritems():
+        if key != "csrfmiddlewaretoken" and key != "bigger":
+            new_word[key] = value
+        if key == "bigger":
+            new_word['big'] = "big"
+        else:
+            new_word['big'] = ""
+    new_word['created_at'] = datetime.now().strftime("%H:%M %p, %B %d, %Y")
+    try:
+        request.session['words']
+    except KeyError:
+        request.session['words'] = []
+
+    temp_list = request.session['words']
+    temp_list.append(new_word)
+    request.session['words'] = temp_list
+    for key, val in request.session.__dict__.iteritems():
+        print key, val
+    print "past craeted at", new_word
+
+    return redirect('/')
+
+def clear(request):
+    for key in request.session.keys():
+        del request.session[key]
+    return redirect('/')
